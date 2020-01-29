ba5632df286df8a2b28a7eed1e6ccd8b5a4b0d57
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-01-29 11:55:47 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      cache: {},
+      breeds: [],
+      imageURLs: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(res => res.json())
+      .then(res => this.setState({ breeds: res.message }))
+  }
+
+  //method to handle change in input and update state
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    // console.log(this.state)
+  }
+
+  //method to take the value from state.searchbox and first search the cache, if not there, use the term to query the API
+    //store results in the cache as well as displaying them.
+  // handleSearch(){
+  //   let value = this.state.value;
+
+
+  //method to pull fetch image URLs and combine them with breeds data in state
+
+  render() {
+    //pull data from this.state.data and display as 
+    console.log('inside the render, this.state.breeds is', typeof this.state.breeds, this.state.breeds)
+    //destructure
+    return (
+      <div>
+        <h1>Breeds</h1>
+        <input type = "text" placeholder='enter breed' value = {this.state.value} onChange={this.handleChange} />
+          <Breeds breeds={this.state.breeds}/>
+      </div>
+    );
+  }
 }
+
+function Breeds(props){
+    let breeds = { props };
+    let list = Object.keys(breeds)
+    let breedList = list.forEach(e => {
+      return <li key={e}>{e}</li>
+    });
+    return (
+      <ul>{breedList}</ul>
+    );
+}
+
+
 
 export default App;
