5beff5d775ffca01afab2187e021d92e34ba0109
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-01-29 18:07:15 -0800

    state caching implemented, bugs in image rendering inside Breed Component

--- src/App.js
+++ src/App.js
@@ -5,51 +5,95 @@ class App extends Component {
     super(props);
     this.state = {
       value: '',
-      cache: {},
       breeds: [],
-      imageURLs: []
+      imageURLs: {},
+      clicked: false
     }
     this.handleChange = this.handleChange.bind(this);
+    this.pairImageRefs = this.pairImageRefs.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount(){
+  //first get the list of breeds for us to check users input against before we make image fetch
+  componentDidMount() {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
-      .then(res => this.setState({ breeds: res.message }))
+      .then(res => this.setState({ breeds: res.message })
+      )
   }
 
-  //method to handle change in input and update state
+  //method to handle change in input 
   handleChange(event) {
     this.setState({value: event.target.value});
-
   }
 
-  //method to take the value from state.searchbox and first search the cache, if not there, use the term to query the API
-    //store results in the cache as well as displaying them.
-  
-  //method to pull fetch image URLs and combine them with breeds data in state for rendering
+  //when the user want to search a breed
+  handleClick(e){
+    let { breeds, value } = this.state;
+    breeds = Object.keys(breeds)
+    // console.log('in handleClick breeds is', breeds)
+    //if breeds doesn't include this.state.value, throw an error
+    if(!breeds.includes(value)) alert('Doggy Not Found');
+    //if value not cached, pairImageURLs
+    if(!this.state.imageURLs[value]){ 
+      this.pairImageRefs();
+    } 
+    this.setState({clicked: true});  
+  }
 
+  pairImageRefs() {
+    let breed = this.state.value;
+    if(!this.state.imageURLs[breed]) {
+      fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        .then(res => res.json())
+        .then(res => {
+          let urls = this.state.imageURLs;
+          urls[breed] = res.message;
+          this.setState({ imageURLs: urls });
+          console.log('updated image cache', this.state)
+          this.setState({clicked: true});
+        })
+        .catch(err => console.log('there was an error fetching images, check input', err))
+    }
+  }
+ 
   render() {
+    let content = null;
+    if(this.state.clicked){
+      content = <Breed breed={this.state.value} urls={this.state.imageURLs[this.state.value]} />
+    }
+  
     return (
       <div>
         <h1>Breeds</h1>
-        <input type = "text" placeholder='enter breed' value = {this.state.value} onChange={this.handleChange} />
-          <Breeds breeds={this.state.breeds}/>
+        <input type = "text" placeholder='enter breed' value={this.state.value} onChange={this.handleChange} />
+        <button onClick={this.handleClick}> Woof!</button>
+        {content}
       </div>
     );
   }
 }
 
-function Breeds(props){
-    let breeds = { props };
-    let list = Object.keys(breeds)
-    let breedList = list.forEach(e => {
-      return <li key={e}>{e}</li>
-    });
-    return (
-      <ul>{breedList}</ul>
-    );
+function Breed(props){
+  let { breed , urls } = props;
+  console.log('in Breed comoonent, urls is', urls, Array.isArray(urls))
+  urls = [urls];
+  let i = 0;
+  console.log('in breed component render', urls)
+  // build pictures out from urls
+  let images = urls.map((e) => {
+    console.log(`e is`, e)
+      return ( <img key={++i} src={e} alt=''/> )
+    }
+  );
+  return (
+    <>
+      {breed}
+      {images} 
+    </>
+  );
 }
+
 
 
 
