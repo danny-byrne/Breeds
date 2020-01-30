be3d5c7bf2894899a62e4a0a8c4538929f0b529f
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-01-29 19:19:20 -0800

    images loading, working out css

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ class App extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.pairImageRefs = this.pairImageRefs.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleUnclick = this.handleUnclick.bind(this);
   }
 
   //first get the list of breeds for us to check users input against before we make image fetch
   componentDidMount() {
     fetch('https://dog.ceo/api/breeds/list/all')
       .then(res => res.json())
-      .then(res => this.setState({ breeds: res.message })
-      )
+      .then(res => this.setState({ breeds: res.message }))
   }
 
   //method to handle change in input 
@@ -27,46 +27,53 @@ class App extends Component {
     this.setState({value: event.target.value});
   }
 
+  handleUnclick(){
+    console.log('unclicked')
+    this.setState({ clicked: false });
+  }
+
   //when the user want to search a breed
   handleClick(e){
     let { breeds, value } = this.state;
     breeds = Object.keys(breeds)
-    // console.log('in handleClick breeds is', breeds)
-    //if breeds doesn't include this.state.value, throw an error
     if(!breeds.includes(value)) alert('Doggy Not Found');
-    //if value not cached, pairImageURLs
     if(!this.state.imageURLs[value]){ 
-      this.pairImageRefs();
+      this.pairImageRefs()
+        .then(() => this.setState({clicked: true}))
+    } else {
+      this.setState({clicked: true})
     } 
-    this.setState({clicked: true});  
   }
 
-  pairImageRefs() {
+  async pairImageRefs() {
     let breed = this.state.value;
-    if(!this.state.imageURLs[breed]) {
-      fetch(`https://dog.ceo/api/breed/${breed}/images`)
-        .then(res => res.json())
-        .then(res => {
-          let urls = this.state.imageURLs;
-          urls[breed] = res.message;
-          this.setState({ imageURLs: urls });
-          console.log('updated image cache', this.state)
-          this.setState({clicked: true});
-        })
-        .catch(err => console.log('there was an error fetching images, check input', err))
-    }
+    fetch(`https://dog.ceo/api/breed/${breed}/images`)
+      .then(res => res.json())
+      .then(res => {
+        let urls = this.state.imageURLs;
+        urls[breed] = res.message;
+        this.setState({ imageURLs: urls });
+        console.log('updated image cache', this.state)
+        this.setState({clicked: true});
+      })
+      .catch(err => console.log('there was an error fetching images, check input', err))
+      .then(() => this.handleUnclick)
   }
  
   render() {
+    let divStyle = {
+      alignContent: 'flex'
+    }
+
     let content = null;
     if(this.state.clicked){
       content = <Breed breed={this.state.value} urls={this.state.imageURLs[this.state.value]} />
     }
   
     return (
-      <div>
+      <div style ={divStyle} >
         <h1>Breeds</h1>
-        <input type = "text" placeholder='enter breed' value={this.state.value} onChange={this.handleChange} />
+        <input type = "text" placeholder='enter breed' value={this.state.value} onChange={this.handleChange} onClick={this.handleUnclick}/>
         <button onClick={this.handleClick}> Woof!</button>
         {content}
       </div>
@@ -75,17 +82,25 @@ class App extends Component {
 }
 
 function Breed(props){
+  let breedStyle = {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    alignItems: 'center',
+    width: "70%"
+  }
+
   let { breed , urls } = props;
-  console.log('in Breed comoonent, urls is', urls, Array.isArray(urls))
-  urls = [urls];
+  console.log('Breed, props.urls is', urls, 'array check:', Array.isArray(urls))
   let i = 0;
-  console.log('in breed component render', urls)
   // build pictures out from urls
-  let images = urls.map((e) => {
-    console.log(`e is`, e)
-      return ( <img key={++i} src={e} alt=''/> )
-    }
-  );
+  let images = null;
+  if(urls){
+    images = urls.map((e) => {
+        return ( <img style={breedStyle} key={++i} src={e} alt=''/> )
+      }
+    );
+  }
   return (
     <>
       {breed}
