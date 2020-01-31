7ca4d83f58ed10480a1388eae9864b9324cb83f1
Danny Byrne <dannybyrne@DANIELs-MacBook-Pro.local>
2020-01-30 20:13:14 -0800

    refactoring JSX picture render

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     this.state = {
       value: '',
       breeds: [],
-      imageURLs: {},
+      images: {},
       clicked: false
     }
     this.handleChange = this.handleChange.bind(this);
@@ -17,12 +17,13 @@ class App extends Component {
     this.fetchDoggies = this.fetchDoggies.bind(this);
     this.updateLocalStorage = this.updateLocalStorage.bind(this);
     this.getLocalStorage = this.getLocalStorage.bind(this);
+    this.getMeDoggies = this.getMeDoggies.bind(this);
   }
   
 
   //if localStorage doesn't have state, 
   componentDidMount() {
-    console.log('in componentDidMount, state is', this.state,'checking local storage')
+    console.log('in componentDidMount, state is ', this.state,'checking local storage')
     // this.fetchDoggies();
     //if local storage does not have state, they are at the beginning of their session and we should fetch the doggies
     if(localStorage.getItem('state') === null ) {
@@ -49,8 +50,8 @@ class App extends Component {
   }
 
   getInitialStorage () {
-      let { breeds, imageUrls } = JSON.parse(localStorage.getItem('state')) 
-      this.setState({ breeds: breeds, imageUrls: imageUrls })
+      let { breeds, images } = JSON.parse(localStorage.getItem('state')) 
+      this.setState({ breeds: breeds, images: images })
   }
 
   getLocalStorage () {
@@ -64,6 +65,7 @@ class App extends Component {
   //method to handle change in input 
   handleChange(event) {
     this.setState({value: event.target.value});
+    this.updateLocalStorage();
   }
 
   handleUnclick(){
@@ -72,18 +74,43 @@ class App extends Component {
   }
 
 
-  //when the user want to search a breed
+  // when the user want to search a breed
   handleClick(e){
-    let { breeds, value } = this.state;
-    if(!breeds.includes(value)) {
+    let { value } = this.state;
+    if(!this.state.breeds.includes(value)) {
       alert('Doggy Not Found');
     }
-    if(!this.state.imageURLs[value]){ 
+    if(!this.state.images[value]){ 
       this.pairImageRefs()
         .then(() => this.setState({clicked: true}))
     } else {
       this.setState({ clicked: true })
     } 
+  }
+  
+  getMeDoggies(){
+    console.log(`value is`, this.state.value.split(',').map(e => e.trim().toLowerCase()))
+    let doggies = this.state.value.split(',').map(e => e.trim());
+    console.log('doggies is', doggies)
+    doggies.forEach((e) => {
+      console.log(`in getMeDoggies e is`, e, 'type:',typeof e)
+      console.log('this.state.breeds is', this.state.breeds)
+      // if(this.state.images[e]){
+      //   console.log(`${e} pictures loaded`)
+      // } else {
+      //   console.log(`not found`)
+      // }
+      if(this.state.breeds.includes(e) && this.state.images[e]){
+        console.log(`creating a breed with ${e}`)
+        return ( <Breed images={this.state.images[e].slice(0,5)} breed={e} />)
+      } else if (this.state.breeds.includes(e) && !this.state.images[e]){
+        this.pairImageRefs(e);
+        // return this.getMeDoggies(e);
+        return ( <Breed images={this.state.images[e].slice(0,5)} breed={e} />)
+      } else {
+        return (<h1>{`${e} is not a valid doggo breed, try again`}</h1>)
+      }
+    })
   }
 
   async pairImageRefs() {
@@ -91,9 +118,9 @@ class App extends Component {
     fetch(`https://dog.ceo/api/breed/${breed}/images`)
       .then(res => res.json())
       .then(res => {
-        let urls = this.state.imageURLs;
+        let urls = this.state.images;
         urls[breed] = res.message;
-        this.setState({ imageURLs: urls });
+        this.setState({ images: urls });
         console.log('updated image cache', this.state)
         this.setState({clicked: true});
         localStorage.setItem('state', JSON.stringify(this.state))
@@ -101,35 +128,36 @@ class App extends Component {
       .catch(err => console.log('there was an error fetching images, check input', err))
       .then(() => this.handleUnclick)
   }
+
+  
  
   render() {
     // let divStyle = {
     //   alignContent: 'flex'
     // }
-    let content = null;
+    // let content = null;
+    let urDoggies = null;
 
     if(this.state.clicked){
-      content = <Breed breed={this.state.value} urls={this.state.imageURLs[this.state.value]} />
-    }
-  
+      urDoggies = this.getMeDoggies()
+    }    
     return (
       <div className={classes.body} >
         <h1 className={classes.text}>Breeds</h1>
         <input className={classes.input} type = "text" placeholder='enter breed' value={this.state.value} onChange={this.handleChange} onClick={this.handleUnclick}/>
-        <button className={classes.button} onClick={this.handleClick}> Click to Search </button>
-        {content}
+        <button className={classes.button} onClick={this.getMeDoggies}> Click to Search </button>
+        {urDoggies}
       </div>
     );
   }
 }
 
-function Breed(props){
-
-  let { breed , urls } = props;
+function Breed (props){
+  let { breed , images } = props;
   let i = 0;
-  let images = null;
-  if(urls){ 
-    images = urls.map((e) => {
+  let gallery = null;
+  if(images){ 
+    gallery = images.map((e) => {
       return ( <img className={classes.pictures} key={++i} src={e} alt=''/> )
     });
   }
@@ -137,7 +165,7 @@ function Breed(props){
   return (
     <>
       <h1 style={classes.h1}>{breed}</h1>
-      {images} 
+      {gallery} 
     </>
   );
 }
